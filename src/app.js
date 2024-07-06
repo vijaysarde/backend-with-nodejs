236d8b3fd5d9dc1e770b584fa89da22a7cf30193
VJay Sarde <88164909+vijaysarde@users.noreply.github.com>
2024-07-06 23:04:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,14 +4,22 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+// to restrict request from unknow website
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
+// Middleware to parse JSON data
 app.use(express.json({limit: "16kb"}))
+
+// Middleware to parse FORM data which is nothing but urlencoded data
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+// Middleware to serve static files from "public" directory
 app.use(express.static("public"))
+
+// Middleware to parse the cookie without this you cannot access `req.cookies`
 app.use(cookieParser())
 
 
@@ -36,7 +44,5 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
-
-// http://localhost:8000/api/v1/users/register
 
 export { app }
